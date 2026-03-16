313f5194333ae90a9bbe3de22593010b28064b1b
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2026-03-16 14:32:42 -0300

    add: Implementa validacao de usuario

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,9 +1,19 @@
 package com.nexus.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
     private final String username;
     private final String email;
 
+    protected static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    protected static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.matches();
+    }
     public User(String username, String email) {
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
