b9c0f17469573228754c5301b758c3ea7f0c60e9
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2026-03-23 12:00:39 -0300

    add: Implementa relatorios na classe Workspace - overloadedUsers() - getGlobalBottlenecks() - getTaskByID()

--- src/main/java/com/nexus/service/Workspace.java
+++ src/main/java/com/nexus/service/Workspace.java
@@ -1,10 +1,12 @@
 package com.nexus.service;
 
 import com.nexus.model.Task;
+import com.nexus.model.TaskStatus;
 import com.nexus.model.User;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.Collections;
 import java.util.Comparator;
@@ -34,4 +36,36 @@ public class Workspace {
         .map(Map.Entry::getKey)
         .toList();
 }
+
+
+    // Retorna uma lista de usuarios com mais de 10 tasks
+    public List<User> overloadedUsers() {
+    Map<User, Long> taskCountByUser = tasks.stream()
+            .collect(Collectors.groupingBy(Task::getOwner, Collectors.counting()));
+
+
+    // Retornando os usuarios com mais de 10 tasks
+    return taskCountByUser.entrySet().stream()
+        .filter(entry -> entry.getValue() > 10)
+        .map(Map.Entry::getKey)
+        .toList();
+    }
+
+
+    public TaskStatus getGlobalBottlenecks() {
+    Map<TaskStatus, Long> taskCountByStatus = tasks.stream()
+            .collect(Collectors.groupingBy(Task::getStatus, Collectors.counting()));
+
+    return taskCountByStatus.entrySet().stream()
+        .sorted(Map.Entry.<TaskStatus, Long>comparingByValue(Comparator.reverseOrder()))
+        .limit(1)
+        .map(entry -> entry.getKey())
+        .findFirst().orElse(null) ;
+}
+
+
+    public Task getTaskById(int id) {
+        Task task = tasks.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        return task;
+    }
 }
