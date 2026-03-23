f8029915a23120da71a8e09cbd21206ecab36551
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2026-03-23 10:40:55 -0300

    add: Top performers report implemented on Workspace class

--- src/main/java/com/nexus/service/Workspace.java
+++ src/main/java/com/nexus/service/Workspace.java
@@ -1,9 +1,13 @@
 package com.nexus.service;
 
 import com.nexus.model.Task;
+import com.nexus.model.User;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace {
     private final List<Task> tasks = new ArrayList<>();
@@ -16,4 +20,18 @@ public class Workspace {
         // Retorna uma visão não modificável para garantir encapsulamento
         return Collections.unmodifiableList(tasks);
     }
+
+    // Retorna uma lista imutavel com os top_n usuarios com mais tasks
+    public List<User> topPerformers(int top_n) {
+    // Criando um HashMap com a relacao de usuarios e qtd de tarefas
+    Map<User, Long> taskCountByUser = tasks.stream()
+            .collect(Collectors.groupingBy(Task::getOwner, Collectors.counting()));
+
+    // Retornando os n usuarios com mais tasks
+    return taskCountByUser.entrySet().stream()
+        .sorted(Map.Entry.<User, Long>comparingByValue(Comparator.reverseOrder()))
+        .limit(top_n)
+        .map(Map.Entry::getKey)
+        .toList();
+}
 }
